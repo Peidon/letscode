ba8dc1a3db19b4811a85a62879e8936eb19758ac
Peidon <peidong.xu@transwarp.io>
2019-11-12 19:06:23 +0800

    2019-11-12

--- levelorder.py
+++ levelorder.py
@@ -62,3 +62,36 @@ class Solution(object):
                 i -= 1
             ret.append(level)
         return ret
+
+    def zigzagLevelOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+
+        ret = []
+        queue = deque()
+
+        if root:
+            queue.append(root)
+        else:
+            return ret
+
+        levelcount = 0
+        while queue:
+            level = []
+            i = len(queue)
+
+            levelcount += 1
+            while(i > 0):
+                node = queue.popleft()
+                level.append(node.val)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                i -= 1
+            if levelcount % 2: ret.append(level)
+            else: ret.append(level[::-1])
+
+        return ret
