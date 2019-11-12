6b507f526606e0c500a6bb7115e12c4aef480e64
Peidon <peidong.xu@transwarp.io>
2019-11-12 19:06:23 +0800

    2019-11-12

--- searchWord.py
+++ searchWord.py
@@ -1,4 +1,9 @@
 class Solution(object):
+
+    def __init__(self):
+        self.visited = None
+        self.result = False
+
     def exist(self, board, word):
         """
         :type board: List[List[str]]
@@ -15,31 +20,24 @@ class Solution(object):
         for i in range(len(board)):
             for j in range(len(board[i])):
                 if board[i][j] == word[0]:
-                    visited = [[False] * len(board[i]) for _ in range(len(board))]
-                    if self.dfs(board, i, j, word, 1, visited):
-                        return True
-        return False
+                    self.visited = [[False] * len(board[i]) for _ in range(len(board))]
+                    self.dfs(board, i, j, word, 1)
+        return self.result
 
-    def dfs(self, board, i, j, word, k, visited):
-        if k == len(word): return True
-        visited[i][j] = True
-        if i - 1 >= 0 and not visited[i - 1][j]:
-            if word[k] == board[i - 1][j]:
-                if self.dfs(board, i - 1, j, word, k + 1, visited):
-                    return True
-        if i + 1 < len(board) and not visited[i + 1][j]:
-            if word[k] == board[i + 1][j]:
-                if self.dfs(board, i + 1, j, word, k + 1, visited):
-                    return True
-        if j - 1 >= 0 and not visited[i][j - 1]:
-            if word[k] == board[i][j - 1]:
-                if self.dfs(board, i, j - 1, word, k + 1, visited):
-                    return True
-        if j + 1 < len(board[i]) and not visited[i][j + 1]:
-            if word[k] == board[i][j + 1]:
-                if self.dfs(board, i, j + 1, word, k + 1, visited):
-                    return True
-        return False
+    def dfs(self, board, i, j, word, k):
+        if k == len(word):
+            self.result = True
+            return
+        self.visited[i][j] = True
+        if i - 1 >= 0:
+            if not self.visited[i - 1][j] and word[k] == board[i - 1][j]: self.dfs(board, i - 1, j, word, k + 1)
+        if i + 1 < len(board):
+            if not self.visited[i + 1][j] and word[k] == board[i + 1][j]: self.dfs(board, i + 1, j, word, k + 1)
+        if j - 1 >= 0:
+            if not self.visited[i][j - 1] and word[k] == board[i][j - 1]: self.dfs(board, i, j - 1, word, k + 1)
+        if j + 1 < len(board[i]):
+            if not self.visited[i][j + 1] and word[k] == board[i][j + 1]: self.dfs(board, i, j + 1, word, k + 1)
+        self.visited[i][j] = False
 
 ans = Solution().exist([["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]],"ABCESEEEFS")
 print(ans)
