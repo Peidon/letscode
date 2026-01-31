76bf43a8ced0ef4345c7748baa234d50a4579b4a
Pedro's Jet <peidong.xu94@gmail.com>
2026-01-31 22:37:23 +1100

    trim code

--- solutions/array/let169_majority_elem.py
+++ solutions/array/let169_majority_elem.py
@@ -1,40 +1,42 @@
 from typing import List
 
 
-class Solution:
+def create_pivot(a: List[int], l: int, r: int) -> int:
+    pivot = a[l]
+    while l < r:
+        while l < r and pivot <= a[r]:
+            r -= 1
+        a[l] = a[r]
 
-    def create_pivot(self, a: List[int], l: int, r: int) -> int:
-        pivot = a[l]
-        while l < r:
-            while l < r and pivot <= a[r]:
-                r -= 1
-            a[l] = a[r]
+        while l < r and a[l] <= pivot:
+            l += 1
+        a[r] = a[l]
 
-            while l < r and a[l] <= pivot:
-                l += 1
-            a[r] = a[l]
+    a[l] = pivot
+    return l
 
-        a[l] = pivot
-        return l
 
-    def majorityElement(self, nums: List[int]) -> int:
+def majorityElement(nums: List[int]) -> int:
+    """
+    # The majority element is the element that appears more than ⌊n / 2⌋ times.
+    # You may assume that the majority element always exists in the array.
+    """
 
-        l, r = 0, len(nums) - 1
-        d = len(nums) // 2
+    l, r = 0, len(nums) - 1
+    d = len(nums) // 2
 
-        while l < r:
-            p = self.create_pivot(nums, l, r)
-            if p > d:
-                r = p - 1
-            elif p < d:
-                l = p + 1
-            else:
-                return nums[p]
+    while l < r:
+        p = create_pivot(nums, l, r)
+        if p > d:
+            r = p - 1
+        elif p < d:
+            l = p + 1
+        else:
+            return nums[p]
 
-        return nums[l]
+    return nums[l]
 
 
 if __name__ == '__main__':
-    s = Solution()
-    m = s.majorityElement([3,3,4])
+    m = majorityElement([3,3,4])
     print(m)
